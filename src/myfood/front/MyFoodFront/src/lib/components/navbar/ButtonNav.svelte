09d807fde89e5efa4ed4e556f39dbcb55be3432d
Breno Brandão <brenoca35@gmail.com>
2024-04-26 18:05:38 -0300

    configuracoes inicias + componente navbar

---
+++ src/myfood/front/MyFoodFront/src/lib/components/navbar/ButtonNav.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { ComponentType } from 'svelte';
+	import type { Icon } from 'lucide-svelte';
+	export let href = '#';
+	export let Icon: ComponentType<Icon> | null = null;
+	export let label = '';
+</script>
+
+<li>
+	<a {href} class="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-red-500 transition ease-in-out hover:text-white">
+		{#if Icon}
+			<svelte:component	
+				this={Icon}
+				class="h-5 w-5"
+			/>
+		{/if}
+		<span class="ms-3 ">{label}</span>
+	</a>
+</li>
