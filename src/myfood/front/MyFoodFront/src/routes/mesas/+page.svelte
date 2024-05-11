0a69621be63e9256d5887091130d7e5db47634bd
LeonardoVieiraM <leonardovieiramachado@gmail.com>
2024-05-11 16:51:03 -0300

    pagina mesas

--- src/myfood/front/MyFoodFront/src/routes/mesas/+page.svelte
+++ src/myfood/front/MyFoodFront/src/routes/mesas/+page.svelte
@@ -1,5 +1,5 @@
-<script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
+<script lang="ts" context="module">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
 </script>
---
+++ src/myfood/front/MyFoodFront/src/routes/mesas/[id].svelte
@@ -0,0 +1,17 @@
+<script context="module">
+  export async function load({ params }) {
+    const { id } = params;
+
+    return {
+      props: {
+        id,
+      },
+    };
+  }
+</script>
+
+<script>
+  export let id;
+</script>
+
+<p>Mesa {id}</p>
