0d2c36e52d074e74ac56dbe7a1f291d623618ab8
Breno Brandão <brenoca35@gmail.com>
2024-05-12 14:37:57 -0300

    routing by id working

--- src/myfood/front/MyFoodFront/src/routes/mesas/[id]/+page.server.ts
+++ src/myfood/front/MyFoodFront/src/routes/mesas/[id]/+page.server.ts
@@ -1,5 +1,10 @@
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad } from "./$types";
 
-export const load = (async () => {
-    return {};
+let endpoint = "http://localhost:8080";
+
+export const load = (async ({ params }) => {
+  const id = params.id;
+  const response = await fetch(`${endpoint}/mesas/${id}`).then((e) => e.json());
+  console.log(response);
+  return { id };
 }) satisfies PageServerLoad;
--- src/myfood/front/MyFoodFront/src/routes/mesas/[id]/+page.svelte
+++ src/myfood/front/MyFoodFront/src/routes/mesas/[id]/+page.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+
+  let mesa = data;
   let CadastroCliente;
+  let endpoint = "http://localhost:8080";
 
   onMount(async () => {
     const module = await import("$lib/components/cards/CadastroCliente.svelte");
     CadastroCliente = module.default;
   });
-
-  let endpoint = "http://localhost:8080";
 </script>
 
 <div class="p-4 sm:ml-64">
-  <h1>Mesa</h1>
+  <h1>Mesa {mesa.id}</h1>
   <div class="flex justify-center items-center">
     {#if CadastroCliente}
       <CadastroCliente />
