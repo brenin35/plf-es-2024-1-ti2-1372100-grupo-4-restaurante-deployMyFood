a208638c0d5841ca59ed8fc9fc85f92df433aa7c
Breno Brandão <brenoca35@gmail.com>
2024-05-11 22:20:10 -0300

    qr code generating V1

---
+++ src/myfood/front/MyFoodFront/src/routes/cardapio/[id]/+page.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+    export async function load({ params }) {
+      const { id } = params;
+  
+      return {
+        props: {
+          id,
+        },
+      };
+    }
+  </script>
+  
+  <script>
+    export let id;
+  </script>
+  
+  <div class="p-4 sm:ml-64">
+    <p>Mesa {id}</p>
+  </div>
+  
--- src/myfood/front/MyFoodFront/src/routes/mesas/+page.svelte
+++ src/myfood/front/MyFoodFront/src/routes/mesas/+page.svelte
@@ -1,5 +1,38 @@
 <script lang="ts" context="module">
-  import type { PageData } from "./$types";
+  // import type { PageData } from "./$types";
 
-  export let data: PageData;
+  // export let data: PageData;
+
+  let inputText = "";
+  let textPresent = false;
+  let API_URL = "https://api.qrserver.com/v1/create-qr-code/?data=";
+  let size = "&size=[150]x[150]";
+  let full_link = "";
+
+  function dataSubmit(e) {
+    if (inputText !== "") {
+      textPresent = true;
+      full_link = API_URL + inputText + size;
+    }
+    console.log(full_link);
+  }
 </script>
+
+<div class="p-4 sm:ml-64">
+  <h1>QR CODE GENERATOR</h1>
+  <form on:submit|preventDefault={dataSubmit}>
+    <input
+      bind:value={inputText}
+      class="textInput"
+      type="text"
+      placeholder="Enter any text or url..."
+    />
+    <input class="btn" type="submit" value="Submit" />
+  </form> 
+  {#if textPresent}
+    <img src={full_link} alt="" /><br />
+    <a href={full_link} download>Download</a>
+  {:else}
+    <p>No QR code yet! ☹️</p>
+  {/if}
+</div>
