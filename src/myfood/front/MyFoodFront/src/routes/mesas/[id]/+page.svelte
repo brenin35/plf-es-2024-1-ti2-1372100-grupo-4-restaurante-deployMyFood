dcd0caf4efaae6d8397610d468df6a2f4f47e7c0
Breno Brandão <brenoca35@gmail.com>
2024-05-12 13:11:29 -0300

    changin place of mesa/id

---
+++ src/myfood/front/MyFoodFront/src/routes/mesas/[id]/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  let CadastroCliente;
+
+  onMount(async () => {
+    const module = await import("$lib/components/cards/CadastroCliente.svelte");
+    CadastroCliente = module.default;
+  });
+
+  let endpoint = "http://localhost:8080";
+</script>
+
+<div class="p-4 sm:ml-64">
+  <h1>Mesa</h1>
+  <div class="flex justify-center items-center">
+    {#if CadastroCliente}
+      <CadastroCliente />
+    {/if}
+  </div>
+</div>
