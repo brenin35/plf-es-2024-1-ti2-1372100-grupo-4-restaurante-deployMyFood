172d423b5b01abd95f35fed5ad53e957415fee65
Breno Brandão <brenoca35@gmail.com>
2024-05-12 14:44:09 -0300

    controller clientes

--- src/myfood/back_novo/src/main/java/com/myfood/myfoodback/controllers/ClientesController.java
+++ src/myfood/back_novo/src/main/java/com/myfood/myfoodback/controllers/ClientesController.java
@@ -1,10 +1,53 @@
 package com.myfood.myfoodback.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.myfood.myfoodback.models.Clientes;
+import com.myfood.myfoodback.repositories.ClientesRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClientesController {
 
+    private ClientesRepository clientesRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Clientes>> getAllClientes() {
+        List<Clientes> clientes = clientesRepository.findAll();
+        return new ResponseEntity<>(clientes, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Clientes> getClienteById(@PathVariable Long id) {
+        Optional<Clientes> cliente = clientesRepository.findById(id);
+        if (cliente.isPresent()) {
+            return new ResponseEntity<>(cliente.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Clientes> createCliente(@RequestBody Clientes cliente) {
+        try {
+            Clientes newCliente = clientesRepository.save(cliente);
+            return new ResponseEntity<>(newCliente, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteCliente(@PathVariable Long id) {
+        try {
+            clientesRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
