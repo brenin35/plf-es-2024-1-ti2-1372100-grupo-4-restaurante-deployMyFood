575b54f6e4c0ce128128f854d57b4bb5e09d1f10
Breno Brandão <brenoca35@gmail.com>
2024-05-12 17:42:47 -0300

    fetch serverside

--- src/myfood/front/MyFoodFront/src/routes/mesas/[id]/+page.server.ts
+++ src/myfood/front/MyFoodFront/src/routes/mesas/[id]/+page.server.ts
@@ -5,6 +5,12 @@ let endpoint = "http://localhost:8080";
 export const load = (async ({ params }) => {
   const id = params.id;
   const response = await fetch(`${endpoint}/mesas/${id}`).then((e) => e.json());
+  const responseClientes = await fetch(`${endpoint}/clientes`).then((e) =>
+    e.json()
+  );
+
+  console.log(responseClientes);
+  console.log(response)
   const { nomeMesa } = response;
   return { id, nomeMesa };
 }) satisfies PageServerLoad;
