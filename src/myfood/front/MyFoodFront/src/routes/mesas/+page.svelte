d7baf4baf7728828d7aa0fc903d7b6b41b366fcd
Breno Brandão <brenoca35@gmail.com>
2024-05-14 16:11:27 -0300

    if else mesa

--- src/myfood/front/MyFoodFront/src/routes/mesas/+page.svelte
+++ src/myfood/front/MyFoodFront/src/routes/mesas/+page.svelte
@@ -2,7 +2,12 @@
   import Button from "$lib/components/ui/button/button.svelte";
   import { onMount } from "svelte";
   import { Diamonds } from "svelte-loading-spinners";
-  import { ENDPOINT_URL, DEPLOY_FRONT_URL, API_URL, SIZE } from "$lib/constants";
+  import {
+    ENDPOINT_URL,
+    DEPLOY_FRONT_URL,
+    API_URL,
+    SIZE,
+  } from "$lib/constants";
   // import type { PageData } from "./$types";
 
   // export let data: PageData;
@@ -68,36 +73,39 @@
         <Diamonds size="60" color="#FF3E00" unit="px" duration="1s" />
       </div>
     {:then}
-      <div
-        class="grid grid-cols-2 gap-5 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 justify-center items-center"
-      >
-        {#each mesas as mesa, index (mesa.id)}
-          <div
-            class="w-full border shadow-lg p-5 flex flex-col gap-2 rounded-lg"
-          >
-            <h2 class="text-center text-xl">{mesa.nomeMesa}</h2>
-            {#await qrLinks}
-              <Diamonds size="60" color="#FF3E00" unit="px" duration="1s" />
-            {:then}
-              <img src={qrLinks[index]} alt="QR Code" /><br />
-            {/await}
-            <div class="flex gap-2">
-              <Button variant="buttonDD">Deletar mesa</Button>
-              <Button variant="buttonDD">
-                <a
-                  class="flex justify-end"
-                  target="_blank"
-                  href={qrLinks[index]}
-                  download="qrcode">Download</a
-                >
-              </Button>
-            </div>
-          </div>
-          {/each}
-        </div>
+      {#if mesas.length == 0}
         <div class="flex justify-center items-center mt-40">
           <h1 class="text-xl text-center">Nenhuma mesa registrada!</h1>
         </div>
+      {:else}
+        <div
+          class="grid grid-cols-2 gap-5 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 justify-center items-center"
+        >
+          {#each mesas as mesa, index (mesa.id)}
+            <div
+              class="w-full border shadow-lg p-5 flex flex-col gap-2 rounded-lg"
+            >
+              <h2 class="text-center text-xl">{mesa.nomeMesa}</h2>
+              {#await qrLinks}
+                <Diamonds size="60" color="#FF3E00" unit="px" duration="1s" />
+              {:then}
+                <img src={qrLinks[index]} alt="QR Code" /><br />
+              {/await}
+              <div class="flex gap-2">
+                <Button variant="buttonDD">Deletar mesa</Button>
+                <Button variant="buttonDD">
+                  <a
+                    class="flex justify-end"
+                    target="_blank"
+                    href={qrLinks[index]}
+                    download="qrcode">Download</a
+                  >
+                </Button>
+              </div>
+            </div>
+          {/each}
+        </div>
+      {/if}
     {/await}
   </div>
 </div>
