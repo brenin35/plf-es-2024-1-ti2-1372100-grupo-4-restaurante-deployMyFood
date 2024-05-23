7c02d90a24dab3c644e10252d2b4f36d5f0d30ca
Breno Brandão <brenoca35@gmail.com>
2024-05-22 21:45:53 -0300

    update dependencies

--- src/myfood/back_novo/src/main/java/com/myfood/myfoodback/s3/S3Service.java
+++ src/myfood/back_novo/src/main/java/com/myfood/myfoodback/s3/S3Service.java
@@ -10,37 +10,42 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 import software.amazon.awssdk.core.sync.RequestBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 @Service
 public class S3Service {
 
+    private static final Logger logger = LoggerFactory.getLogger(S3Service.class);
     private final S3Client s3Client;
     private final String bucketName;
 
     public S3Service(@Value("${aws.accessKeyId}") String accessKeyId,
-                     @Value("${aws.secretKey}") String secretKey,
-                     @Value("${aws.s3.region}") String region,
-                     @Value("${aws.s3.bucket-name}") String bucketName) {
+            @Value("${aws.secretKey}") String secretKey,
+            @Value("${aws.s3.region}") String region,
+            @Value("${aws.s3.bucket-name}") String bucketName) {
         this.bucketName = bucketName;
         AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKeyId, secretKey);
         this.s3Client = S3Client.builder()
-                                .region(Region.of(region))
-                                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-                                .build();
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .build();
     }
 
     public String uploadFile(MultipartFile file) throws IOException {
         String key = file.getOriginalFilename();
 
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                                                            .bucket(bucketName)
-                                                            .key(key)
-                                                            .build();
+                .bucket(bucketName)
+                .key(key)
+                .build();
 
-        PutObjectResponse response = s3Client.putObject(putObjectRequest, 
-                                                        RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+        PutObjectResponse response = s3Client.putObject(putObjectRequest,
+                RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+        logger.info("File uploaded to S3 with key: {} and response: {}", key, response);
 
         return s3Client.utilities().getUrl(b -> b.bucket(bucketName).key(key)).toExternalForm();
     }
