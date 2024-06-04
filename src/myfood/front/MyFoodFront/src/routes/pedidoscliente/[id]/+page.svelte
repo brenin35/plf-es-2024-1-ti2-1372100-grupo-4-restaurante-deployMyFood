37f55f9203d669e280092a29f68e8ed9c746ac53
Breno Brandão <brenoca35@gmail.com>
2024-06-04 11:04:20 -0300

    pagamento pix funcionando

--- src/myfood/front/MyFoodFront/src/routes/pedidoscliente/[id]/+page.svelte
+++ src/myfood/front/MyFoodFront/src/routes/pedidoscliente/[id]/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { loadStripe } from '@stripe/stripe-js';
+  import { toast } from "svelte-sonner";
+  import { onMount } from "svelte";
+  import { loadStripe } from "@stripe/stripe-js";
   import type { Pedido } from "$lib/types.ts";
   import ModalAvaliar from "$lib/components/modal/ModalAvaliar.svelte";
   import type { PageData } from "./$types";
   import { pedidoStore } from "$lib/stores/pedidoStore";
   import Button from "$lib/components/ui/button/button.svelte";
+  import * as Dialog from "$lib/components/ui/dialog/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+  import { Label } from "$lib/components/ui/label/index.js";
+  import { ENDPOINT_URL } from "$lib/constants";
 
   export let data: PageData;
 
@@ -23,24 +28,35 @@
   });
 
   onMount(async () => {
-    stripe = await loadStripe('pk_test_51PNExaFK72ovkSt8je6WKkEbVPo3elT8UNSGhn9UDoPEhePTefYBOUHJc2PSJcxc42iiMEdB0ppNT4KNP0BMu7nN00kHfuRIlO');
+    stripe = await loadStripe(
+      "pk_test_51PNExaFK72ovkSt8je6WKkEbVPo3elT8UNSGhn9UDoPEhePTefYBOUHJc2PSJcxc42iiMEdB0ppNT4KNP0BMu7nN00kHfuRIlO"
+    );
   });
 
   async function handlePayment(pedido) {
-    const response = await fetch('http://localhost:8080/api/payment/create-checkout-session', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ amount: pedido.precoTotalPedido * 100 }), // Amount in cents
-    });
+    const response = await fetch(
+      `${ENDPOINT_URL}/api/payment/create-checkout-session`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ amount: pedido.precoTotalPedido * 100 }), // Amount in cents
+      }
+    );
 
     if (response.ok) {
       const { id } = await response.json();
       stripe.redirectToCheckout({ sessionId: id });
     } else {
-      console.error('Failed to create checkout session');
+      console.error("Failed to create checkout session");
     }
+  }
+
+  function notify() {
+    toast.success("Vá até o caixa", {
+      description: "Vá até o caixa para realizar o pagamento com o atendente!",
+    });
   }
 </script>
 
@@ -59,25 +75,60 @@
         {pedido.statusPagamento ? "Pago" : "Não Pago"}
       </p>
       <p>
-        <strong>Preço Total do Pedido:</strong> R${pedido.precoTotalPedido.toFixed(2)}
+        <strong>Preço Total do Pedido:</strong>
+        R${pedido.precoTotalPedido.toFixed(2)}
       </p>
     </div>
 
     <h2 class="text-xl font-semibold mb-3">Itens do Pedido</h2>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+    <div
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
+    >
       {#each pedido.itensPedido as itemPedido}
-        <div class="flex flex-col items-center justify-center gap-4 border p-4 rounded">
+        <div
+          class="flex flex-col items-center justify-center gap-4 border p-4 rounded"
+        >
           <p><strong>Produto ID:</strong> {itemPedido.produto.id}</p>
           {#if itemPedido.produto.nome}
             <p><strong>Nome do Produto:</strong> {itemPedido.produto.nome}</p>
           {/if}
           <p><strong>Quantidade:</strong> {itemPedido.quantidade}</p>
-          <p><strong>Preço Item:</strong> R${itemPedido.precoItem.toFixed(2)}</p>
-          <p><strong>Preço Total:</strong> R${itemPedido.precoTotal.toFixed(2)}</p>
+          <p>
+            <strong>Preço Item:</strong> R${itemPedido.precoItem.toFixed(2)}
+          </p>
+          <p>
+            <strong>Preço Total:</strong> R${itemPedido.precoTotal.toFixed(2)}
+          </p>
           <ModalAvaliar produtoId={itemPedido.produto.id} />
         </div>
       {/each}
     </div>
-    <Button on:click={() => handlePayment(pedido)}>Realizar pagamento</Button>
+    <div class="mt-10">
+      <Dialog.Root>
+        <Dialog.Trigger>
+          <Button variant="buttonAdd">Escolher forma de pagamento</Button>
+        </Dialog.Trigger>
+        <Dialog.Content class="sm:max-w-[600px]">
+          <Dialog.Header>
+            <Dialog.Title>Escolha a forma de pagamento</Dialog.Title>
+            <Dialog.Description>Escolha!</Dialog.Description>
+          </Dialog.Header>
+          <div class="grid gap-4 py-4">
+            <div class="grid grid-cols-4 items-center gap-4">
+              <Button variant="buttonAdd" class="w-full" on:click={notify}>Dinheiro</Button>
+              <Button variant="buttonAdd" class="w-full" on:click={() => handlePayment(pedido)}>Cartão</Button>
+              <a href="/pagamentopix/{pedido.id}" class="w-full">
+                <Button class="w-full" variant="buttonAdd">Pix</Button>
+              </a>
+            </div>
+          </div>
+          <!-- <Dialog.Footer>
+            <Dialog.Close>
+              <Button variant="buttonAdd" type="submit">Avaliar</Button>
+            </Dialog.Close>
+          </Dialog.Footer> -->
+        </Dialog.Content>
+      </Dialog.Root>
+    </div>
   </div>
 {/each}
