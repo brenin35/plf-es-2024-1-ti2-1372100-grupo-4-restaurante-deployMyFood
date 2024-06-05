b0882c0984ed1fd1ceaa9267d9c5f5a75b5ad24e
Breno Brandão <brenoca35@gmail.com>
2024-06-05 10:03:41 -0300

    fetch mesas on server side updating instant

--- src/myfood/front/MyFoodFront/src/routes/mesas/+page.svelte
+++ src/myfood/front/MyFoodFront/src/routes/mesas/+page.svelte
@@ -15,7 +15,6 @@
   let cardapioMesa = `${DEPLOY_FRONT_URL}/mesas`;
   let mesas: Mesa[] = data.mesas;
   let qrLinks: string[] = [];
-  let promise = fetch(`${ENDPOINT_URL}/mesas`);
 
   type Mesa = {
     id: number;
@@ -33,8 +32,8 @@
   async function handleDeleteMesa(id: number) {
     try {
       await deleteMesa(id);
-      await mesas;
-      window.location.reload();
+      
+      mesas = mesas.filter(mesa => mesa.id !== id);
     } catch (error) {
       console.error("Erro deletando mesa:", error);
     }
@@ -42,8 +41,8 @@
 
   async function handleCreateMesa() {
     try {
-      await criarMesa();
-      await mesas;
+      const newMesa = await criarMesa();
+      mesas = [...mesas, newMesa];
     } catch (error) {
       console.error("Erro criando mesa:", error);
     }
@@ -59,46 +58,40 @@
       >Cadastrar mesa</Button
     >
   </div>
-  {#await promise}
+  <!-- {#await promise}
     <div class="flex items-center justify-center mt-20">
       <Diamonds size="60" color="#FF3E00" unit="px" duration="1s" />
     </div>
-  {:then}
-    {#if mesas.length == 0}
-      <div class="flex justify-center items-center mt-40">
-        <h1 class="text-xl text-center">Nenhuma mesa registrada!</h1>
-      </div>
-    {:else}
-      <div
-        class="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 justify-center items-center"
-      >
-        {#each mesas as mesa, index (mesa.id)}
-          <div
-            class="w-full border shadow-lg p-5 flex flex-col gap-2 rounded-lg"
-          >
-            <h2 class="text-center text-xl">Mesa {mesa.id}</h2>
-            {#await qrLinks}
-              <Diamonds size="60" color="#FF3E00" unit="px" duration="1s" />
-            {:then}
-              <img src={qrLinks[index]} alt="QR Code" /><br />
-            {/await}
-            <div class="flex gap-2 flex-col xl:flex-row">
-              <Button
-                variant="buttonDD"
-                on:click={() => handleDeleteMesa(mesa.id)}>Deletar mesa</Button
+  {:then} -->
+  {#if mesas.length == 0}
+    <div class="flex justify-center items-center mt-40">
+      <h1 class="text-xl text-center">Nenhuma mesa registrada!</h1>
+    </div>
+  {:else}
+    <div
+      class="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 justify-center items-center"
+    >
+      {#each mesas as mesa, index (mesa.id)}
+        <div class="w-full border shadow-lg p-5 flex flex-col gap-2 rounded-lg">
+          <h2 class="text-center text-xl">Mesa {mesa.id}</h2>
+          <img src={qrLinks[index]} alt="QR Code" /><br />
+          <div class="flex gap-2 flex-col xl:flex-row">
+            <Button
+              variant="buttonDD"
+              on:click={() => handleDeleteMesa(mesa.id)}>Deletar mesa</Button
+            >
+            <Button variant="buttonDD">
+              <a
+                class="flex justify-end"
+                target="_blank"
+                href={qrLinks[index]}
+                download="qrcode">Download</a
               >
-              <Button variant="buttonDD">
-                <a
-                  class="flex justify-end"
-                  target="_blank"
-                  href={qrLinks[index]}
-                  download="qrcode">Download</a
-                >
-              </Button>
-            </div>
+            </Button>
           </div>
-        {/each}
-      </div>
-    {/if}
-  {/await}
+        </div>
+      {/each}
+    </div>
+  {/if}
+  <!-- {/await} -->
 </div>
