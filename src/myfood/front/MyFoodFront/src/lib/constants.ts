35b73d82d4a347295f309c1c70724189c2d210b4
Breno Brandão <brenoca35@gmail.com>
2024-06-08 10:21:45 -0300

    hook +server

--- src/myfood/front/MyFoodFront/src/lib/constants.ts
+++ src/myfood/front/MyFoodFront/src/lib/constants.ts
@@ -1,5 +1,5 @@
-//export const ENDPOINT_URL = "https://plf-es-2024-1-ti2-1372100-grupo-4.onrender.com";
-export const ENDPOINT_URL = "http://localhost:8080";
+export const ENDPOINT_URL = "https://plf-es-2024-1-ti2-1372100-grupo-4.onrender.com";
+//export const ENDPOINT_URL = "http://localhost:8080";
 export const DEPLOY_FRONT_URL = "https://plf-es-2024-1-ti2-1372100-grupo-4-restaurante-deploy-my-food.vercel.app";
 export const API_URL = "https://api.qrserver.com/v1/create-qr-code/?data=";
 export const SIZE = "&size=150x150";
