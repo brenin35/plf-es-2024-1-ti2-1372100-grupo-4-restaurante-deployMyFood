b4903b9784787a7ad027832e78aae3ac0b2a6e69
Breno Brandão <brenoca35@gmail.com>
2024-06-08 11:17:31 -0300

    page cancelado

---
+++ src/myfood/front/MyFoodFront/src/routes/user/pedidocancelado/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let clienteId: number;
+
+  onMount(() => {
+    const storedClienteId = localStorage.getItem("clienteId");
+
+    if (storedClienteId) {
+      clienteId = parseInt(storedClienteId, 10);
+      if (!isNaN(clienteId)) {
+        alertUser();
+      } else {
+        console.error("Cliente ID is not a valid number");
+      }
+    } else {
+      console.error("Cliente não registrado");
+    }
+  });
+
+  function alertUser() {
+    const message =
+      "Seu pagamento foi cancelado. Clique OK para retornar à pagina de pagamento!";
+    if (window.confirm(message)) {
+      window.location.href = `/user/pedidoscliente/${clienteId}`;
+    }
+  }
+</script>
