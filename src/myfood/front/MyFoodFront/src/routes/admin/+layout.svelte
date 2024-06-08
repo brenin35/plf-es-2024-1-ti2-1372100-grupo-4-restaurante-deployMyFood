199b148a9d87e807881ebfd51a9ba4674276ab5e
Breno Brandão <brenoca35@gmail.com>
2024-06-08 10:27:25 -0300

    layouts admin/user

---
+++ src/myfood/front/MyFoodFront/src/routes/admin/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+    import "../app.pcss";
+    import Navbar from "$lib/components/navbar/Navbar.svelte";
+    import { Toaster } from "$lib/components/ui/sonner";
+  
+  </script>
+  
+  <div class="app">
+    <main>
+      <Navbar />
+      <div class="p-4 sm:ml-64">
+        <Toaster />
+        <slot></slot>
+      </div>
+    </main>
+  </div>
+  
---
+++ src/myfood/front/MyFoodFront/src/routes/user/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+    import NavbarCliente from "$lib/components/navbar/NavbarCliente.svelte";
+    import "../app.pcss";
+    import { Toaster } from "$lib/components/ui/sonner";
+  
+  </script>
+  
+  <div class="app">
+    <main>
+      <NavbarCliente />
+      <div class="p-4 sm:ml-64">
+        <Toaster />
+        <slot></slot>
+      </div>
+    </main>
+  </div>
+  
